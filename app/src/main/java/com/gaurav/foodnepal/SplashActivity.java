4cfa92144fa386f74f17fa356ab3ff65ca1ce9a3
Sabin RanaBhat <sabin@Sabins-MacBook-Pro.local>
2019-01-07 20:26:28 +0545

    splash activity with animation

--- app/src/main/java/com/gaurav/foodnepal/SplashActivity.java
+++ app/src/main/java/com/gaurav/foodnepal/SplashActivity.java
@@ -1,13 +1,40 @@
 package com.gaurav.foodnepal;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private RelativeLayout splashRL;
+
+    private final int interval = 4000; // 4 Second
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable(){
+        public void run() {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        splashRL = findViewById(R.id.splashRL);
+        Animation popUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.pop_up);
+        splashRL.startAnimation(popUp);
+
+
+        handler.postAtTime(runnable, System.currentTimeMillis()+interval);
+        handler.postDelayed(runnable, interval);
     }
 }
