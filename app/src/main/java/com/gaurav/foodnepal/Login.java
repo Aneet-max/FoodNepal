5b9627883b62bb5eefcafaea33acf88e6e3eb949
Gaurav Baral <madrid.fever.gb@gmail.com>
2019-01-07 21:27:32 +0545

    Login activity added

---
+++ app/src/main/java/com/gaurav/foodnepal/Login.java
@@ -0,0 +1,82 @@
+package com.gaurav.foodnepal;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.gaurav.foodnepal.Common.Common;
+import com.gaurav.foodnepal.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Login extends AppCompatActivity {
+
+    EditText edtPhone, edtPassword;
+    Button btnSignIn, btnSignUp;
+    CheckBox saveLoginCheckBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtPhone = (EditText) findViewById(R.id.edtPhone);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        saveLoginCheckBox = (CheckBox) findViewById(R.id.checkbox1);
+
+        //tala ko ley firebase ra app connect gaok ok
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+                mDialog.setMessage("Please waiting");
+                mDialog.show();
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                            //User ko barey info haru
+                            mDialog.dismiss();
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                            user.setPhone(edtPhone.getText().toString());
+                            if (user.getPassword().equals(edtPassword.getText().toString())) {
+
+                                Intent intent = new Intent(MainActivity.this, Home.class);
+                                Common.currentUser = user;
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            mDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "User doesnot exist", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
