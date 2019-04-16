5d6009f5d173f8334c7a67b7bf91771e79af30d9
Sabin <sawin0>
2019-04-16 13:35:09 +0545

    refactored

--- app/src/main/java/com/gaurav/foodnepal/PlaceDetail.java
+++ app/src/main/java/com/gaurav/foodnepal/PlaceDetail.java
@@ -14,7 +14,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ListView;
